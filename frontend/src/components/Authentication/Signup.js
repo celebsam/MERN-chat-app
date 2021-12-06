596b6694d1e9ffe75d05be01e9f557833d6cadf4
Samuel Ogbe-Green <samuelogbe0@gmail.com>
2021-12-06 23:07:37 +0100

    Added cloudinary

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -28,7 +28,7 @@ const Signup = () => {
       <Toast>
         <Toast.Header>
           <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
-          <strong className="me-auto">Bootstrap</strong>
+          <strong className="me-auto">Error</strong>
           <small>11 mins ago</small>
         </Toast.Header>
         <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
